c0c058ca6f355aa58dd314120115be178eaf9b12
TacosConChelas <adriherandezor@gmail.com>
2024-04-18 19:14:49 -0600

    Este es el primer coommit del repositorio, si lo pueden ver en su git log quiere decir que hicieron todo bien

---
+++ PruebaRepo.java
@@ -0,0 +1,8 @@
+public class PruebaRepo {
+    public static void main(String[] args) {
+        System.out.println("Hola este es solo un mensjae para probar si todo esta bien\nEspero que todos se encuentren bien");
+        //Si estan biendo esto en us computadoras quiere decir que todo a salido bien ajajaajaj felicidades
+        // <3
+    }
+    
+}
